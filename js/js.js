e792acb233e9633dcd9902760402708182e3fb97
karimkabirov <karim3487@gmail.com>
2021-09-26 18:16:27 +0300

    commit

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+let block = document.querySelector('.collapsible')
 let coll = document.getElementsByClassName("collapsible");
 for (let i = 0; i < coll.length; i++) {
     coll[i].addEventListener('click', function() {
@@ -6,12 +7,18 @@ for (let i = 0; i < coll.length; i++) {
         console.log(content);
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
+            block.style.backgroundColor = '#cccccc';
+            block.style.width = '';
+            block.style.borderRadius = '15px';
         } else {
             content.style.maxHeight = content.scrollHeight + 'px';
+            block.style.backgroundColor = '#ababab';
+            block.style.width = '100%';
+            block.style.borderBottomLeftRadius = '0';
+            block.style.borderBottomRightRadius = '0';
         }
     })
 }
 
-console.log(coll.style)
 
 
