e6dcd8cbca0a37339a53b445194d7e3b71ba054c
karim3487 <karim3487@gmail.com>
2021-10-23 17:23:52 +0300

    validate with js

--- js/validPassword.js
+++ js/validPassword.js
@@ -1,18 +1,24 @@
-var password = document.getElementById('password');
-var secondPassword = document.getElementById('secondpassword');
-var form = document.getElementById('form');
-var passwordInput = document.getElementById("passwordInput")
+let password = document.querySelector("input[name='password']");
+let secondPassword = document.querySelector("input[name='secondpassword']");
+let form = document.getElementById('form');
+// let passwordInput = document.getElementById("passwordInput")
 
-var newElement = document.createElement("p");
-var newText = document.createTextNode("Пароли не совпадают");
+let newElement = document.createElement("p");
+let newText = document.createTextNode("Пароли не совпадают");
 newElement.appendChild(newText);
 
 newElement.className = "invalidPass";
 
+let firstPass = password.value;
+let secPass = secondPassword.value;
+
 
 function validatePassword() {
+    console.log(password.value);
+    console.log(secondPassword.value);
+
     if (form.reportValidity()) {
-        if (password !== secondPassword) {
+        if (password.value !== secondPassword.value) {
             passwordInput.appendChild(newElement)
         } else {
             form.submit();
